17390e8f4ce78ec3e3eb4285925bb63707e41354
vsanchezrod <v.sanchez.rod@gmail.com>
2018-07-18 15:06:51 +0200

    Creando proyecto SpotiApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ArtistaComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
